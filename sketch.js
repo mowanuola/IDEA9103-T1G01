71377225868fc6a3cb0110cc9371cc5de77860a2
Akin-Jimoh Mowanuola <akinjimohmowa@gmail.com>
2023-10-24 14:18:38 +1100

    new: started working on abstract implementation of art

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+let colors = ["#FFFFFF", "#D40907", "#1356A2", "#F7D300", "#CCCCCC"];
+// let widths = [20,30, 50, 10,70,]
+let minSize = 30,
+  maxSize = 90;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
+  noLoop();
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  noStroke();
+  let x = 0;
+  while (x < height) {
+    let y = 0;
+
+    while (y < height) {
+      let boxWidth = random(minSize, maxSize);
+      let boxHeight = random(minSize, maxSize);
+
+      let noiseColor = noise(x * 0.05, y * 0.05) * colors.length;
+      fill(colors[floor(noiseColor)]);
+
+      rect(x, y, boxWidth, boxHeight);
+
+      y += boxHeight;
+    }
+
+    x += maxSize;
+  }
+  // for (let x = 0; x < width; x += 60) {
+  //   for (let y = 0; y < height; y += 60) {
+  //     let selectedColor = random(colors);
+  //     fill(selectedColor);
+  //     rect(x, y, 60);
+  //     noStroke();
+  //   }
+  // }
 }
