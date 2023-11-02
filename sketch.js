05881ea1e9a9fc3d4fdd2429bfdd07c39e4de9f8
Akin-Jimoh Mowanuola <akinjimohmowa@gmail.com>
2023-11-02 22:08:15 +1100

    edit: added grid lines and updated rectangles class

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 let colors = ["#cc3300", "#cc6600", "#cc9966", "#669999"];
 let columns = 0;
-let boxWidth = 30;
+let boxWidth = 20;
 let x;
 let y;
-
+let innerRects = [];
+let availableOffsets = [2.4, 4.2, 3, 0.3, 1.15, 0.6, 4.5, 0.6];
 let rectangles = [];
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,17 +16,38 @@ function setup() {
 
 function draw() {
   background(238, 239, 233, 255);
+  createGrid();
+  for (let rect of rectangles) {
+    rect.draw();
+  }
+  rectMode(CORNER);
+  fill(238, 239, 233, 255);
+  rect(290, boxWidth / 2, boxWidth, boxWidth * columns);
+  rect(370, boxWidth / 2, boxWidth, boxWidth * columns);
+  rect(690, boxWidth / 2, boxWidth, boxWidth * columns);
+  rect(1190, boxWidth / 2, boxWidth, boxWidth * columns);
+  rect(1270, boxWidth / 2, boxWidth, boxWidth * columns);
+  rect(boxWidth / 2, 290, boxWidth * columns, boxWidth);
+  rect(boxWidth / 2, 370, boxWidth * columns, boxWidth);
+  rect(boxWidth / 2, 870, boxWidth * columns, boxWidth);
+  rect(boxWidth / 2, 590, boxWidth * columns, boxWidth);
+  rect(370, 690, boxWidth * 16, boxWidth);
+  rect(690, 150, boxWidth * 16, boxWidth);
+  rect(990, 160, boxWidth, boxWidth * 11);
+  rect(1090, 610, boxWidth, boxWidth * 13);
+  rect(1230, 610, boxWidth, boxWidth * 13);
+  rect(1270, 990, boxWidth * 26, boxWidth);
+  rect(130, boxWidth / 2, boxWidth, boxWidth * 14);
+}
 
+function createGrid() {
   for (x = boxWidth; x < width; x += boxWidth) {
+    columns++;
     for (y = boxWidth; y < height; y += boxWidth) {
       let rectangle;
       rectangle = new Rectangle(x, y, boxWidth);
       rectangles.push(rectangle);
     }
-    columns++;
-  }
-  for (let rect of rectangles) {
-    rect.draw();
   }
 }
 
@@ -40,17 +61,33 @@ class Rectangle {
     x,
     y,
     w,
+    h = w,
     color = colors[floor(noise(x * 0.05, y * 0.05) * colors.length)]
   ) {
     this.xPos = x;
     this.yPos = y;
     this.w = w;
-   
+    this.h = h;
     this.color = color;
   }
 
   draw() {
+    // stroke(random(colors));
+    // strokeWeight(2);
     fill(this.color);
-    rect(this.xPos, this.yPos, this.w, );
+    rect(this.xPos, this.yPos, this.w, this.h);
   }
+}
+
+function rectanglesIntersect(recta1, recta2) {
+  return (
+    ((recta1.i <= recta2.i && recta1.i + recta1.si > recta2.i) ||
+      (recta2.i <= recta1.i && recta2.i + recta2.si > recta1.i)) &&
+    ((recta1.j <= recta2.j && recta1.j + recta1.sj > recta2.j) ||
+      (recta2.j <= recta1.j && recta2.j + recta2.sj > recta1.j))
+  );
+}
+
+function roundValueUp(value) {
+  return ceil(value / boxWidth) * boxWidth;
 }
